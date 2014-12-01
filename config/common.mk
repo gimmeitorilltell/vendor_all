8bd3513cc59502147e5c2ca51114cf2c2912def2
scott <scott@ubuntu>
2014-11-30 20:48:05 -0500

    remove a few of the custom CM packages
    Change-Id: Ieb596da99c68e94df7f478f750787d4688b1985a
    Signed-off-by: scott <scott@ubuntu>

--- config/common.mk
+++ config/common.mk
@@ -157,10 +157,7 @@ PRODUCT_PACKAGES += \
     audio_effects.conf \
     Apollo \
     CMFileManager \
-    LockClock \
-    CMUpdater \
-    CMAccount \
-    CMHome
+    LockClock
 
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
