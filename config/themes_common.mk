63871a19784d9391f3001a6a98ee5c397627d670
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-03-31 19:19:22 -0400

    fix left over derpage

--- config/themes_common.mk
+++ config/themes_common.mk
@@ -4,10 +4,5 @@ PRODUCT_PACKAGES += \
     ThemesProvider
 
 PRODUCT_COPY_FILES += \
-<<<<<<< HEAD
-    vendor/candy5/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
-
-=======
     vendor/beanstalk/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
     vendor/beanstalk/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
->>>>>>> 373bd90... Change from CM naming to BeanStalk.  Long Overdue.
